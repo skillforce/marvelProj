e206fef5b30e0426418dcf0ad493f4547ae31cd6
skillforce <skillforce@mail.ru>
2021-11-13 22:00:42 +0300

    to Heroku and Gh-pages commit

--- src/components/charList/char/Char.tsx
+++ src/components/charList/char/Char.tsx
@@ -1,29 +1,31 @@
 import './char.scss';
 import abyss from '../../../resources/img/abyss.jpg';
-import {Component} from 'react';
+import React, {Component} from 'react';
 
 
 type CharPropsType = {
     name: string | null
     img: string | null
-    setSelectedChar: (id: number|null) => void
-    id:number|null
-    charId:number|null
+    setSelectedChar: (id: number | null) => void
+    id: number | null
+    charId: number | null
 }
 
 
 class Char extends Component<CharPropsType> {
 
-
     render() {
-        const {name, img, setSelectedChar,id,charId} = this.props
+        const {name, img, setSelectedChar, id, charId} = this.props
         const notAvailableImg = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
         const correctImgClass = img === notAvailableImg ? 'char__badItem' : 'char__item'
-        const activeItem = charId===id? img === notAvailableImg? 'char__badItem char_selected' : 'char__item char_selected' :correctImgClass
+        const activeItem = charId === id ? img === notAvailableImg ? 'char__badItem char_selected' : 'char__item char_selected' : correctImgClass
+
 
         return (
-            <li onClick={()=>setSelectedChar(id)}  className={activeItem}>
-                <img  src={img ? img : abyss} alt="abyss"/>
+            <li onClick={() => {
+                setSelectedChar(id)
+            }} className={activeItem}>
+                <img src={img ? img : abyss} alt="abyss"/>
                 <div className="char__name">{name ? name : 'not found'}</div>
             </li>
         )
